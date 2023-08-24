2e19a9d954120bc077ffba0fd8a6ca9427df18a1
Solemark <larco9mason@gmail.com>
2023-08-24 21:47:48 +1000

    remove unnecessary whitespace

--- console_app/calculator.dart
+++ console_app/calculator.dart
@@ -1,7 +1,4 @@
 double addition(double a, double b) => a + b;
-
 double subtraction(double a, double b) => a - b;
-
 double multiplication(double a, double b) => a * b;
-
 double division(double a, double b) => a / b;
