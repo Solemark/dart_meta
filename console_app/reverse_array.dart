5b4926557ff1b5e9e4b2b0dbc65f4a093031e7d4
Solemark <larco9mason@gmail.com>
2023-08-27 04:23:01 +1000

    create reverse array

---
+++ console_app/reverse_array.dart
@@ -0,0 +1,25 @@
+List<int> reverseSameArray(List<int> input) {
+  int x = 0;
+  int y = 0;
+  int c = input.length - 1;
+  for (int i = 0; i < c; i++, c--) {
+    x = input[i];
+    y = input[c];
+    input[i] = y;
+    input[c] = x;
+  }
+  return input;
+}
+
+List<int> reverseArray(List<int> input) {
+  List<int> output = [];
+  for (int i = input.length - 1; i >= 0; i--) {
+    output.add(input[i]);
+  }
+  return output;
+}
+
+void main() {
+  print(reverseArray([5, 4, 3, 2, 1, 0]));
+  print(reverseSameArray([5, 4, 3, 2, 1, 0]));
+}
