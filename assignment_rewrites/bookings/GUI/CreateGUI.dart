92958b47252cead2ae838bd5dffe34d8ddeac009
Solemark <larco9mason@gmail.com>
2023-12-20 22:44:25 +1000

    formatting update

--- assignment_rewrites/bookings/GUI/CreateGUI.dart
+++ assignment_rewrites/bookings/GUI/CreateGUI.dart
@@ -4,16 +4,17 @@ import 'BookingGUI.dart';
 class CreateGUI {
   BookingGUI run(BookingGUI gui) {
     Luxury item = new Luxury(
-        gardenArea: int.parse(gui.gardenArea.value ?? '0'),
-        numberOfWeeks: int.parse(gui.numberOfWeeks.value ?? '0'),
-        rooms: int.parse(gui.rooms.value ?? '0'),
-        address: gui.address.value ?? '',
-        bookingDate: gui.bookingDate.value ?? '',
-        bookingID: gui.bookingID.value ?? '',
-        contactNumber: gui.contactNumber.value ?? '',
-        propertyOwnerName: gui.propertyOwnerName.value ?? '',
-        securityAlarmCheck: gui.securityAlarmCheck.checked ?? false,
-        poolMaintenance: gui.poolMaintenance.checked ?? false);
+      gardenArea: int.parse(gui.gardenArea.value ?? '0'),
+      numberOfWeeks: int.parse(gui.numberOfWeeks.value ?? '0'),
+      rooms: int.parse(gui.rooms.value ?? '0'),
+      address: gui.address.value ?? '',
+      bookingDate: gui.bookingDate.value ?? '',
+      bookingID: gui.bookingID.value ?? '',
+      contactNumber: gui.contactNumber.value ?? '',
+      propertyOwnerName: gui.propertyOwnerName.value ?? '',
+      securityAlarmCheck: gui.securityAlarmCheck.checked ?? false,
+      poolMaintenance: gui.poolMaintenance.checked ?? false,
+    );
 
     gui.bookingList.add(item);
     gui.bookings.clear(gui);
