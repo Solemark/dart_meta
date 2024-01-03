fd43cd6e3e4dca38685a7b402d8127ecb68f6da2
Solemark <larco9mason@gmail.com>
2024-01-03 16:54:00 +1000

    clean up routing

--- genshin_impact_api/main.dart
+++ genshin_impact_api/main.dart
@@ -4,22 +4,10 @@ import 'package:shelf/shelf.dart';
 import 'package:shelf/shelf_io.dart' as shelf_io;
 import 'package:shelf_router/shelf_router.dart' as shelf_router;
 
-Future<void> main() async {
-  final int port = 8080;
-  final cascade = Cascade().add(_router.call);
-  final server = await shelf_io.serve(
-    logRequests().addHandler(cascade.handler),
-    InternetAddress.anyIPv4,
-    port,
-  );
-  print('Serving at http://${server.address.host}:${server.port}');
-}
-
 final _router = shelf_router.Router()
   ..get("/", _generalHandler)
-  ..get("/characters/<name>", _characterHandler)
-  ..get("/artifacts/<name>", _artifactHandler)
-  ..get("/weapons/<name>", _weaponHandler);
+  ..get("/<type>", _typeHandler)
+  ..get("/<type>/<name>", _nameHandler);
 
 final _jsonHeaders = {
   'content-type': 'application/json',
@@ -36,12 +24,31 @@ Response _generalHandler(Request request) => Response(
         "weapons": "/weapons",
       }),
     );
-Future<dynamic> _getCharacter(String name) async => jsonDecode(await File("data/characters/$name.json").readAsString());
-Future<dynamic> _getArtifact(String name) async => jsonDecode(await File("data/artifacts/$name.json").readAsString());
-Future<dynamic> _getWeapon(String name) async => jsonDecode(await File("data/weapons/$name.json").readAsString());
-Future<Response> _characterHandler(Request request, String name) async =>
-    Response(200, headers: _jsonHeaders, body: _jsonEncode(await _getCharacter(name)));
-Future<Response> _artifactHandler(Request resuest, String name) async =>
-    Response(200, headers: _jsonHeaders, body: _jsonEncode(await _getArtifact(name)));
-Future<Response> _weaponHandler(Request resuest, String name) async =>
-    Response(200, headers: _jsonHeaders, body: _jsonEncode(await _getWeapon(name)));
+
+Future<List<String>> _getListData(String type) async {
+  List<String> res = await Directory("data/$type")
+      .list()
+      .map((element) => element.toString().split("/").last.replaceAll(".json'", ""))
+      .toList();
+  res.sort();
+  return res;
+}
+
+Future<Response> _typeHandler(Request request, String type) async =>
+    Response(200, headers: _jsonHeaders, body: _jsonEncode(await _getListData(type)));
+
+Future<String> _getData(String type, String name) async => await File("data/$type/$name.json").readAsString();
+
+Future<Response> _nameHandler(Request request, String type, String name) async =>
+    Response(200, headers: _jsonHeaders, body: await _getData(type, name));
+
+Future<void> main() async {
+  final int port = 8080;
+  final cascade = Cascade().add(_router.call);
+  final server = await shelf_io.serve(
+    logRequests().addHandler(cascade.handler),
+    InternetAddress.anyIPv4,
+    port,
+  );
+  print('Serving at http://${server.address.host}:${server.port}');
+}
