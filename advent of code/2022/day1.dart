ecc86f30613eaeba6834d106f49fc4f5ca0a9f61
Solemark <larco9mason@gmail.com>
2024-02-20 22:04:50 +1000

    clean up _getTotal

--- advent of code/2022/day1.dart
+++ advent of code/2022/day1.dart
@@ -1,11 +1,11 @@
-(Iterable<num>, int) _emptyRow(Iterable<num> totals, num total) => ([...totals, total], 0);
 num _formatString(Iterable<num> totals, num total) => _maxKey([...totals, total], [...totals, total].reduce(_max));
+num _getTotal(num total, String item) => item != "" ? _add(total, num.parse(item)) : 0;
 num _maxKey(Iterable<num> totals, num total) => totals.toList().indexOf(total) + 1;
-num _max(num a, num b) => a > b ? a : b;
-num _add(num a, num b) => a + b;
+num _max(num x, num y) => x > y ? x : y;
+num _add(num x, num y) => x + y;
+(Iterable<num>, int) _emptyRow(Iterable<num> totals, num total) => ([...totals, total], 0);
 num day1(Iterable<String> data, Iterable<num> totals, [num total = 0, int i = 0]) {
   if (data.length - 1 <= i) return _formatString(totals, total);
   if (data.elementAt(i) == "") (totals, total) = _emptyRow(totals, total);
-  if (data.elementAt(i) != "") total = _add(total, num.tryParse(data.elementAt(i)) ?? 0);
-  return day1(data, totals, total, i + 1);
+  return day1(data, totals, _getTotal(total, data.elementAt(i)), i + 1);
 }
