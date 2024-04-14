2b6a8912b0e5d1f4d105008929a8f81b2535622d
Solemark <larco9mason@gmail.com>
2024-04-14 10:33:11 +1000

    simplify test

--- console_app/csv_test.dart
+++ console_app/csv_test.dart
@@ -4,40 +4,27 @@ import 'package:csv/csv.dart';
 import 'package:test/test.dart';
 import 'csv.dart';
 
-(String, List<List<String>>) constructor() => (
-      'test.csv',
-      [
-        ['Header 1', 'Header 2', 'Header 3'],
-        ['Data 1-1', 'Data 2-1', 'Data 3-1'],
-        ['Data 1-2', 'Data 2-2', 'Data 3-2']
-      ]
-    );
-
-Future<void> destructor(CSV csv) async => await csv.destroy;
-
 void main() {
-  CSV? csv = null;
+  const String filename = "test.csv";
+  const List<List<String>> data = [
+    ["Header 1", "Header 2", "Header 3"],
+    ["Data 1-1", "Data 2-1", "Data 3-1"],
+    ["Data 1-2", "Data 2-2", "Data 3-2"]
+  ];
 
   setUp(() async {
-    var (filename, data) = constructor();
-    csv = new CSV(filename, data);
-    await csv!.write;
+    write_csv(filename, data);
   });
 
   tearDown(() async {
-    await destructor(csv!);
+    await destroy_csv(filename);
   });
 
   group('Test Write to CSV', () {
-    test('Test Filename Exists', () async => expect(true, await File('${csv!.filename}').exists()));
+    test('Test Filename Exists', () async => expect(true, await File("$filename").exists()));
     test(
         'Test File Has Data',
-        () async => expect(
-            csv!.data,
-            await File('${csv!.filename}')
-                .openRead()
-                .transform(utf8.decoder)
-                .transform(CsvToListConverter())
-                .toList()));
+        () async => expect(data,
+            await File('${filename}').openRead().transform(utf8.decoder).transform(CsvToListConverter()).toList()));
   });
 }
