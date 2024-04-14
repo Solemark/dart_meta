cb4f583a2f4ed61b7c65039c4d1dc8b830056133
Solemark <larco9mason@gmail.com>
2024-04-14 10:32:54 +1000

    make module instead of class based

--- console_app/csv.dart
+++ console_app/csv.dart
@@ -1,11 +1,7 @@
 import 'dart:io';
 import 'package:csv/csv.dart';
 
-class CSV {
-  String filename;
-  List<List<String>> data;
-  CSV(this.filename, this.data);
+Future<void> write_csv(String filename, List<List<String>> data) async =>
+    await File('${filename}').writeAsString(ListToCsvConverter().convert(data));
 
-  Future<void> get write async => await File('${this.filename}').writeAsString(ListToCsvConverter().convert(this.data));
-  Future<void> get destroy async => await File('${this.filename}').delete();
-}
+Future<void> destroy_csv(String filename) async => await File('${filename}').delete();
